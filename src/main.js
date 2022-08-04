7b32f227f4f82adfadb4b29fadd08c8dba28bc1f
zack <zackgakuo@gmail.com>
2022-08-04 10:06:06 +0300

    added variables and some sentences in console log

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-console.log("Hello world");
+//console.log("Hello world");
 
 let age = 30;
 let f_name = 'John';
-
-console.log(`${age} ${f_name}`);
+let isCool = true;
+let place = 'Nairobi';
+let z;
+let n = null;
+// template string
+console.log(`My name is ${f_name} and I am ${age} years old`);
+console.log(`I live in ${place} and it is ${isCool} that I am cool`);
+console.log(typeof isCool);
