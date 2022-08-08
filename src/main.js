555bf7c5280ebbdfe6b0f12a3eb36c438ce02424
zack <zackgakuo@gmail.com>
2022-08-08 11:47:49 +0300

    constructor functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 // object oriented programming
+
+// constructor functions
+
+function Person(first_name,last_name,dob){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.dob = new Date(dob);
+}
+
+john = new Person('John','Doe','12-02-1992'); 
+console.log(john);
+
+frank = new Person('Frank', 'DiCaprio', '3-6-1978');
+console.log(frank);
+
+console.log(frank.dob.getFullYear());
+console.log((john.dob))
