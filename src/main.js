3569efa61ab9c935fdbf8aed016ebb75fd9818e6
zack <zackgakuo@gmail.com>
2022-08-10 10:07:26 +0300

    constructor functions

--- src/main.js
+++ src/main.js
@@ -6,7 +6,14 @@ function Person(first_name,last_name,dob){
     this.first_name = first_name;
     this.last_name = last_name;
     this.dob = new Date(dob);
+
+    this.getBirthyear = () => this.dob.getFullYear();
+
+    
 }
+
+Person.prototype.getFullName = function() {return `${this.first_name} ${this.last_name}`;}
+
 
 john = new Person('John','Doe','12-02-1992'); 
 console.log(john);
@@ -16,3 +23,7 @@ console.log(frank);
 
 console.log(frank.dob.getFullYear());
 console.log((john.dob))
+
+console.log(john.getBirthyear());
+console.log(john.getFullName());
+
