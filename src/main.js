369083dc926790665853595bbf03756f87d7deb1
zack <zackgakuo@gmail.com>
2022-08-10 11:19:14 +0300

    playing with event listeners

--- src/main.js
+++ src/main.js
@@ -30,10 +30,21 @@ ul.children[0].innerHTML = '<h1>LOUD</h1>';
 const btn =  document.querySelector('.btn');
 btn.style.background = 'red';
 
-// event listener to change style
+// event listener on button
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
+    // document.querySelector('#my-form').style.background = '#ccc';
+    // document.querySelector('body').classList.add('bg-dark');
+
+    document.querySelector('.items').children[1].textContent = 'Goodbye';
+    ul.children[2].textContent = 'Fernando';
+
+});
+
+ul.addEventListener('mouseover',(e)=>{
+    ul.children[0].innerHTML = '<p><i>Quiet</i></p>';
+});
+ul.addEventListener('mouseout',(e)=>{
+    ul.children[0].innerHTML = '<h1>LOUD</h1>';
 });
